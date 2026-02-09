bf61c860e0dfa089f250f21a4efe11ab6497ceb2
efrencodes <efrencodes@gmail.com>
2026-02-08 17:33:54 -0800

    feat: Enhance Favorite and Home screens with EmptyState component and favorite toggle functionality

--- app/(tabs)/favorite.tsx
+++ app/(tabs)/favorite.tsx
@@ -1,10 +1,38 @@
+import useFavoriteStore from '@/features/favorites/store/useFavoriteStore'
+import JobsCard from '@/features/jobs/components/JobsCard'
+import { DetailJob } from '@/features/jobs/type'
+import EmptyState from '@/features/shared/components/EmptyState'
 import LayoutBase from '@/features/shared/components/LayoutBase'
-import { Text } from 'react-native'
 
 export default function FavoriteScreen() {
+	const { favorites, setFavorite } = useFavoriteStore()
+
+	const onHandleFavorite = (job: DetailJob) => {
+		const isFavorite = job.isFavorite
+		const newJob = { ...job, isFavorite: !isFavorite }
+		setFavorite(newJob)
+	}
+
 	return (
 		<LayoutBase>
-			<Text>Favorite</Text>
+			{Array.isArray(favorites) &&
+				favorites.length > 0 &&
+				favorites.map((job) => (
+					<JobsCard
+						key={job.id}
+						job={job}
+						isFavorite={job.isFavorite}
+						onToggleFavorite={() => onHandleFavorite(job)}
+					/>
+				))}
+
+			{Array.isArray(favorites) && favorites.length === 0 && (
+				<EmptyState
+					icon="search-outline"
+					title="No jobs found"
+					message="Try adjusting your filters"
+				/>
+			)}
 		</LayoutBase>
 	)
 }
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,5 +1,55 @@
-import { View } from 'react-native'
+import useFavoriteStore from '@/features/favorites/store/useFavoriteStore'
+import { jobsAPI } from '@/features/jobs/api/jobs'
+import JobsCard from '@/features/jobs/components/JobsCard'
+import useJobStore from '@/features/jobs/store/useJobStore'
+import { DetailJob } from '@/features/jobs/type'
+import EmptyState from '@/features/shared/components/EmptyState'
+import LayoutBase from '@/features/shared/components/LayoutBase'
+import { useEffect } from 'react'
 
 export default function HomeScreen() {
-	return <View>Home Page</View>
+	const { setFavorite } = useFavoriteStore()
+	const { jobs, setJob } = useJobStore()
+
+	const getJobList = async () => {
+		try {
+			const response = await jobsAPI.getJobs()
+			setJob(response.data.jobs)
+		} catch (error) {
+			return
+		}
+	}
+
+	useEffect(() => {
+		getJobList()
+	}, [])
+
+	const onHandleFavorite = (job: DetailJob) => {
+		const isFavorite = job.isFavorite
+		const newJob = { ...job, isFavorite: !isFavorite }
+		setFavorite(newJob)
+	}
+
+	return (
+		<LayoutBase>
+			{Array.isArray(jobs) &&
+				jobs.length > 0 &&
+				jobs.map((job) => (
+					<JobsCard
+						key={job.id}
+						job={job}
+						isFavorite={job.isFavorite}
+						onToggleFavorite={() => onHandleFavorite(job)}
+					/>
+				))}
+
+			{Array.isArray(jobs) && jobs.length === 0 && (
+				<EmptyState
+					icon="search-outline"
+					title="No jobs found"
+					message="Try adjusting your filters"
+				/>
+			)}
+		</LayoutBase>
+	)
 }
