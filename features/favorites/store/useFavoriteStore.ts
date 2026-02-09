119d2908147222523354a18200849c678f4e0986
efrencodes <efrencodes@gmail.com>
2026-02-08 21:44:03 -0800

    feat: Refactor useFavoriteStore to include hydration state and persist favorites

--- features/favorites/store/useFavoriteStore.ts
+++ features/favorites/store/useFavoriteStore.ts
@@ -1,28 +1,47 @@
 import { DetailJob } from '@/features/jobs/type'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
-export interface CardState {
+export interface FavoriteState {
 	favorites: DetailJob[]
+	hasHydrated: boolean
 	setFavorite: (favorite: DetailJob) => void
 	resetFavorite: () => void
+	setHasHydrated: (state: boolean) => void
 }
 
-const useFavoriteStore = create<CardState>((set, get) => ({
-	favorites: [],
-	setFavorite: (favorite: DetailJob) =>
-		set((state) => {
-			const isExists = state.favorites.some((f) => f.id === favorite.id)
-			if (isExists)
-				return {
-					favorites: state.favorites.filter(
-						(f) => f.id !== favorite.id,
-					),
-				}
-			return {
-				favorites: [...state.favorites, favorite],
-			}
+const useFavoriteStore = create<FavoriteState>()(
+	persist(
+		(set, get) => ({
+			favorites: [],
+			hasHydrated: false,
+			setFavorite: (favorite: DetailJob) =>
+				set((state) => {
+					const isExists = state.favorites.some(
+						(f) => f.id === favorite.id,
+					)
+					if (isExists)
+						return {
+							favorites: state.favorites.filter(
+								(f) => f.id !== favorite.id,
+							),
+						}
+					return {
+						favorites: [...state.favorites, favorite],
+					}
+				}),
+			resetFavorite: () => set({ favorites: [] }),
+			setHasHydrated: (state: boolean) => set({ hasHydrated: state }),
 		}),
-	resetFavorite: () => set({ favorites: [] }),
-}))
+		{
+			name: 'favorites-storage',
+			storage: createJSONStorage(() => AsyncStorage),
+			onRehydrateStorage: () => (state) => {
+				state?.setHasHydrated(true)
+			},
+		},
+	),
+)
 
 export default useFavoriteStore
