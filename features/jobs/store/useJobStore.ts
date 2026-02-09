8c72acde4f521fc09d80aa07e77329ffa7be01c7
efrencodes <efrencodes@gmail.com>
2026-02-08 19:29:19 -0800

    feat: Add categories on Store

--- features/jobs/store/useJobStore.ts
+++ features/jobs/store/useJobStore.ts
@@ -1,15 +1,19 @@
-import { DetailJob } from '@/features/jobs/type'
+import { Category, DetailJob } from '@/features/jobs/type'
 import { addIsFavorite } from '@/features/jobs/utils/transformData'
 import { create } from 'zustand'
 
 export interface CardState {
 	jobs: DetailJob[]
+	categories: Category[]
+	setCategories: (categories: Category[]) => void
 	setJob: (jobs: DetailJob[]) => void
 	resetJob: () => void
 }
 
 const useJobStore = create<CardState>((set, get) => ({
 	jobs: [],
+	categories: [],
+	setCategories: (categories: Category[]) => set({ categories: categories }),
 	setJob: (jobs: DetailJob[]) =>
 		set((_) => {
 			return { jobs: addIsFavorite(jobs) }
